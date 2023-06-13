12efd8806d01af6b00159c1cf9138b7d276875ef
Adam Getchell <acgetchell@ucdavis.edu>
2023-06-13 07:05:05 -0500

    Add tests, update to use blas_src

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+extern crate blas_src;
 extern crate lapack;
-extern crate openblas_src;
 use lapack::*;
 
 fn main() {
@@ -19,5 +19,31 @@ fn main() {
     assert!(info == 0);
     for (one, another) in w.iter().zip(&[2.0, 2.0, 5.0]) {
         assert!((one - another).abs() < 1e-14);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use lapack::dsyev;
+
+    #[test]
+    fn it_works() {
+        let n = 3;
+        let mut a = vec![3.0, 1.0, 1.0, 1.0, 3.0, 1.0, 1.0, 1.0, 3.0];
+        let mut w = vec![0.0; 4 * n as usize];
+        let mut work = vec![0.0; 4 * n as usize];
+        let lwork = 4 * n;
+        let mut info = 0;
+
+        unsafe {
+            dsyev(
+                b'V', b'U', n, &mut a, n, &mut w, &mut work, lwork, &mut info,
+            );
+        }
+
+        assert!(info == 0);
+        for (one, another) in w.iter().zip(&[2.0, 2.0, 5.0]) {
+            assert!((one - another).abs() < 1e-14);
+        }
     }
 }
